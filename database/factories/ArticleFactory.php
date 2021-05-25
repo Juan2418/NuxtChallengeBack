e45692a0583325ee1589fa31a915573eafb48802
Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>
2021-05-25 14:48:31 -0300

    Create article seeder, factory, model, migration and blank controller

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Article::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $content = $this->faker->realText();
+
+        return [
+            'user_id' => User::all()->random(1)->id,
+            'title' => $this->faker->titleMale,
+            'description' => $this->faker->realText(),
+            'content' => $content,
+            'word_count' => strlen($content),
+            'read_time' => $this->faker->numberBetween(1, 40)
+        ];
+    }
+}
